03a1a0d2a9abff7dba82ab71d34bb87f08fe9355
gprog39 <pyfes@aol.com>
2017-05-23 17:15:45 +0200

    installation approval tests et nunit

---
+++ GhostBuster2Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+    namespace GhostBuster2Tests
+{
+    [TestFixture]
+    public class ghostBuster2UnitTests
+    {
+       
+    }
+}
